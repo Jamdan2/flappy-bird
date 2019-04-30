bd4f430bfc35a1397bde93b8d6f8a64af21e009f
Aidan Heller <asfheller@gmail.com>
2019-04-30 13:49:24 -0700

    Add game screen

--- core/src/main/kotlin/com/jamdan2/flappybird/Game.kt
+++ core/src/main/kotlin/com/jamdan2/flappybird/Game.kt
@@ -1,6 +1,14 @@
 package com.jamdan2.flappybird
 
+import com.jamdan2.flappybird.screens.GameScreen
 import ktx.app.KtxGame
 import ktx.app.KtxScreen
 
-object Game : KtxGame<KtxScreen>()
+object Game : KtxGame<KtxScreen>() {
+    const val WIDTH = 2400f
+    const val HEIGHT = 1600f
+
+    override fun create() {
+        setScreen<GameScreen>()
+    }
+}
---
+++ core/src/main/kotlin/com/jamdan2/flappybird/screens/GameScreen.kt
@@ -0,0 +1,5 @@
+package com.jamdan2.flappybird.screens
+
+import ktx.app.KtxScreen
+
+class GameScreen : KtxScreen
