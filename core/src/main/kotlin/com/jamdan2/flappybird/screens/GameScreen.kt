846ba46a42f6184d500a1550c163e57587f4a534
Aidan Heller <asfheller@gmail.com>
2019-05-01 14:01:38 -0700

    Add bird

--- core/src/main/kotlin/com/jamdan2/flappybird/screens/GameScreen.kt
+++ core/src/main/kotlin/com/jamdan2/flappybird/screens/GameScreen.kt
@@ -2,10 +2,15 @@ package com.jamdan2.flappybird.screens
 
 import com.badlogic.gdx.scenes.scene2d.Stage
 import com.jamdan2.flappybird.Game
+import com.jamdan2.flappybird.actors.Bird
 import ktx.app.KtxScreen
 
 class GameScreen(val game: Game) : KtxScreen {
     val stage = Stage()
+
+    init {
+        stage.addActor(Bird())
+    }
 
     override fun render(delta: Float) {
         stage.act(delta)
