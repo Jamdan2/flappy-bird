ca69903dbff9d7f767874fbb662b80e8438003f0
Aidan Heller <asfheller@gmail.com>
2019-05-10 12:48:10 -0700

    Add engine to game screen

--- core/src/main/kotlin/com/jamdan2/flappybird/screens/GameScreen.kt
+++ core/src/main/kotlin/com/jamdan2/flappybird/screens/GameScreen.kt
@@ -1,18 +1,25 @@
 package com.jamdan2.flappybird.screens
 
+import com.badlogic.ashley.core.Engine
 import com.badlogic.gdx.scenes.scene2d.Stage
 import com.jamdan2.flappybird.Game
-import com.jamdan2.flappybird.actors.Bird
 import ktx.app.KtxScreen
 
 class GameScreen(val game: Game) : KtxScreen {
     val stage = Stage()
 
+    val engine = Engine()
+
     init {
-        stage.addActor(Bird())
+        // Add systems to engine here
+    }
+
+    private fun update(delta: Float) {
+        engine.update(delta)
     }
 
     override fun render(delta: Float) {
+        update(delta)
         stage.act(delta)
         stage.draw()
     }
